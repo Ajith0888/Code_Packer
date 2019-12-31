3785243f726b82fa487c4d01fd00e7e61c0a493a
Ajith K M <ajithkumar02@gmail.com>
2019-12-31 01:02:28 +0100

    Fixed the Issue in Jenkins script

--- Packer/variables/shell/RHEL/jenkins.sh
+++ Packer/variables/shell/RHEL/jenkins.sh
@@ -4,6 +4,11 @@ sleep 300
 sudo yum update -y
 sudo yum install -y java
 sudo yum install -y wget
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
--- Packer/variables/shell/Suse/jenkins.sh
+++ Packer/variables/shell/Suse/jenkins.sh
@@ -2,6 +2,11 @@
 #This script is helps to install jenkins on Suse
 sleep 300
 sudo zypper up -y
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
 sudo zypper addrepo -G -f http://pkg.jenkins.io/opensuse-stable/ jenkins
 sudo zypper install -y jenkins
 sudo systemctl start jenkins.service
